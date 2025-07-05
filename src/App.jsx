01e8ba107effb6d2ed208c45f3c68c4d1f7cdfb9
harshnugget <76251920+harshnugget@users.noreply.github.com>
2025-07-05 13:17:51 +0100

    feat(App, Card): Separated Card component & css styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import "./App.css";
+import Card from "./Card";
 
 function getRandomItemsFromArray(array, count = 10) {
   const result = [];
@@ -12,55 +13,6 @@ function getRandomItemsFromArray(array, count = 10) {
   }
 
   return result;
-}
-
-function Card({ name, url, handleReplace, handleShuffle }) {
-  const [image, setImage] = useState(null);
-
-  useEffect(() => {
-    let ignore = false;
-
-    async function fetchImage() {
-      // Check localStorage first
-      const cached = localStorage.getItem(`pokemonImage-${name}`);
-      if (cached) {
-        setImage(cached);
-        return;
-      }
-
-      try {
-        const response = await fetch(url);
-        if (!response.ok) {
-          throw new Error(`Response status: ${response.status}`);
-        }
-
-        const json = await response.json();
-        const image = json.sprites?.other?.["official-artwork"]?.front_default;
-
-        if (!image) {
-          console.error("No image found. Replacing Pokemon...");
-          handleReplace();
-        } else if (!ignore) {
-          localStorage.setItem(`pokemonImage-${name}`, image);
-          setImage(image);
-        }
-      } catch (error) {
-        console.error(error.message);
-      }
-    }
-
-    fetchImage();
-
-    return () => {
-      ignore = true;
-    };
-  }, [name, url, handleReplace]);
-
-  return (
-    <div className="card" onClick={handleShuffle}>
-      {image ? <img src={image} alt={name} /> : <p>Loading...</p>}
-    </div>
-  );
 }
 
 function App() {
